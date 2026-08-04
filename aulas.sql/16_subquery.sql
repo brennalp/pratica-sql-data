6f132a4881664a22862a7e2058258555a45658b9
Brenna <blp@cesar.school>
2026-08-04 08:42:40 -0300

    subquery class part.2

--- aulas.sql/16_subquery.sql
+++ aulas.sql/16_subquery.sql
@@ -1,6 +1,7 @@
 -- quantos clientes chegaram ao quinto dia de sql?
+-- não é a melhor opção por ser uma tabela grande
 
-SELECT count(DISTINCT IdCliente)
+SELECT count(DISTINCT IdCliente) AS qtdCliente
 
 FROM transacoes AS t1
 
@@ -11,4 +12,15 @@ WHERE t1.IdCliente IN (
     WHERE substr(DtCriacao, 1, 10) = '2025-08-25' 
 )
 
-AND substr(t1.DtCriacao, 1, 10) = '2025-08-29' --filtro para ir ate o quinto dia
+AND substr(t1.DtCriacao, 1, 10) = '2025-08-29'; --filtro para ir ate o quinto dia
+
+
+SELECT * 
+
+FROM (
+    SELECT *
+    FROM transacoes
+    WHERE DtCriacao >= '2025-01-01'
+)
+
+WHERE DtCriacao < 2025-07-01;
